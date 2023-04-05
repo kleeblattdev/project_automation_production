dd9cfe1b277583bf6d5adbc9ce39134a71f3c931
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-04-05 09:50:34 +0200

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import "./config/config.js";
+
+const PORT = process.env.PORT || 9999;
+
+const app = express();
+
+//logger
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/api/v1/money", (req, res) => {});
+
+app.get("/api/v1/workload", (req, res) => {});
+
+app.post("api/v1/sell", (req, res) => {});
+
+app.post("api/v1/humans", (req, res) => {});
+
+app.listen(PORT, () => console.log("Server listening on port", PORT));

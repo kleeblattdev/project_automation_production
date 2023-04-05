d2bd483ee395d3aac53cdcd862d63a18ff8fe5c9
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-04-05 13:50:28 +0200

    can add resource and sell

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import "./config/config.js";
+import { readFile, updateKontostand, updateWorkload } from "./helper.js";
 
 const PORT = process.env.PORT || 9999;
 
@@ -12,16 +13,31 @@ app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
-app.use(cors({ origin: "http://localhost:5137" }));
+app.use(cors());
 
 app.use(express.json());
 
-app.get("/api/v1/money", (req, res) => {});
+app.get("/api/v1/money", (req, res) => {
+	readFile()
+		.then((data) => res.json(data))
+		.catch((err) => console.log(err));
+});
 
-app.get("/api/v1/workload", (req, res) => {});
+app.get("/api/v1/workload", (req, res) => {
+	readFile()
+		.then((data) => res.json(data))
+		.catch((err) => console.log(err));
+});
 
-app.post("api/v1/sell", (req, res) => {});
+app.post("/api/v1/sell", (req, res) => {
+	updateKontostand();
+	res.end();
+});
 
-app.post("api/v1/humans", (req, res) => {});
+app.post("/api/v1/humans/:resource", (req, res) => {
+	const resource = req.params.resource;
+	updateWorkload(resource);
+	res.end();
+});
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
